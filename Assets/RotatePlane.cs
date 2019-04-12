1e23d7ce4dc262e6bd70ecc6567125eb4d4ae7b3
evanwilt <ev.wilt@gmail.com>
2019-04-11 20:31:26 -0400

    Started building a wind gust

---
+++ Assets/RotatePlane.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatePlane : MonoBehaviour
+{
+    public Rigidbody rb;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("TurnLeft")) {
+            transform.Rotate(0, -1, 0, Space.Self);
+            print(transform.rotation.eulerAngles.y);
+            if (transform.rotation.eulerAngles.y <= -45) {
+                transform.localRotation = Quaternion.Euler(0, -45, 0);
+            }
+        }
+
+        if (Input.GetButton("TurnRight")) {
+            transform.Rotate(0, 1, 0, Space.Self);
+            if (transform.rotation.eulerAngles.y >= 45) {
+                transform.localRotation = Quaternion.Euler(0, 45, 0);
+            }
+        }
+    }
+}
---
+++ Assets/WindController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindController : MonoBehaviour
+{
+    public GameObject plane;
+    public Renderer rend;
+    public Collider coll;
+    public bool enter = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        plane = GameObject.Find("Cube");
+        rend = GetComponent<Renderer>();
+        coll = GetComponent<Collider>();
+        rend.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        Rigidbody planeRb = plane.GetComponent<Rigidbody>();
+        planeRb.AddForce(0, 1, 0, ForceMode.Impulse);
+    }
+}
