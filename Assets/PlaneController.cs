bc26fe9a54d6437546657b57f210d50b7f3f469e
Nboruff <nboruff86@gmail.com>
2019-04-16 21:24:28 -0400

    Add files via upload

--- Assets/PlaneController.cs
+++ Assets/PlaneController.cs
@@ -7,45 +7,63 @@ public class PlaneController : MonoBehaviour
 {
     public TextMeshProUGUI speedText;
     float lift, drag;
+    bool launched = false;
     Rigidbody body;
+    
+
     // Start is called before the first frame update
     void Start()
     {
         body = GetComponent<Rigidbody>();
-        body.AddForce(new Vector3(0, 0, -20), ForceMode.Impulse);
+        body.isKinematic = true;
     }
 
     // Update is called once per frame
     void Update()
     {   
-        if(Input.GetAxis("Horizontal") > 0) {
-            body.AddTorque(0,0,10);
+        if(launched == false){
+            if(Input.GetAxis("Vertical") > 0){
+                body.transform.Rotate(.5f,0,0);
+            }
+            if(Input.GetAxis("Vertical") < 0){
+                body.transform.Rotate(-.5f,0,0);
+            }
+            if(Input.GetKeyDown("space")){
+                body.isKinematic = false;
+                body.AddRelativeForce(Vector3.back * 40, ForceMode.Impulse);
+                launched = true;
+            }
         }
-        else if(Input.GetAxis("Horizontal") < 0) {
-            body.AddTorque(0,0,-10);
-        }
-        if(Input.GetAxis("Vertical") > 0) {
-            body.AddTorque(-10,0,0);
-        }
-        else if(Input.GetAxis("Vertical") < 0) {
-            body.AddTorque(10,0,0);
-        }
-        float velocity = body.velocity.magnitude;
-        //lift = 0.5 * (air density)(v^2)(wing area)(coefficient of lift)
-        Vector3 localVelocity = transform.InverseTransformDirection(body.velocity);
-        float angleOfAttack = Mathf.Atan2(-localVelocity.y, localVelocity.z);
-        float lift = (0.5f * velocity * velocity) * angleOfAttack;
-        float drag = 0.8f * (0.5f * velocity * velocity);
-        var dragDirection = -body.velocity.normalized;
-        var liftDirection = Vector3.Cross(dragDirection, transform.right);
-        Debug.DrawLine(transform.position, transform.position - transform.forward*10, Color.green);
-        Debug.DrawLine(transform.position, transform.position + dragDirection*10, Color.blue);
-        Debug.DrawLine(transform.position, transform.position + body.velocity, Color.red);
+        else if(launched == true){            
+            if(Input.GetAxis("Horizontal") > 0) {
+                body.AddTorque(0,0,10);
+            }
+            else if(Input.GetAxis("Horizontal") < 0) {
+                body.AddTorque(0,0,-10);
+            }
+            if(Input.GetAxis("Vertical") > 0) {
+                body.AddTorque(-10,0,0);
+            }
+            else if(Input.GetAxis("Vertical") < 0) {
+                body.AddTorque(10,0,0);
+            }
+            float velocity = body.velocity.magnitude;
+            //lift = 0.5 * (air density)(v^2)(wing area)(coefficient of lift)
+            Vector3 localVelocity = transform.InverseTransformDirection(body.velocity);
+            float angleOfAttack = Mathf.Atan2(-localVelocity.y, localVelocity.z);
+            float lift = (0.5f * (velocity * velocity)) * angleOfAttack;
+            float drag = .3f * (0.5f * (velocity * velocity));
+            var dragDirection = -body.velocity.normalized;
+            var liftDirection = Vector3.Cross(dragDirection, transform.right);
+            Debug.DrawLine(transform.position, transform.position - transform.forward*10, Color.green);
+            Debug.DrawLine(transform.position, transform.position + dragDirection*10, Color.blue);
+            Debug.DrawLine(transform.position, transform.position + body.velocity, Color.red);
 
-        //creating vectors
-        Vector3 liftVector = liftDirection * lift;
-        Vector3 dragVector = dragDirection * drag;
-        body.AddForce(liftVector + dragVector, ForceMode.Force);
-        speedText.text = "Airspeed = " + velocity + "\n Vertical Airspeed = " + body.velocity.y;
+            //creating vectors
+            Vector3 liftVector = liftDirection * lift;
+            Vector3 dragVector = dragDirection * drag;
+            body.AddForce(liftVector + dragVector, ForceMode.Force);
+            speedText.text = "Airspeed = " + velocity + "\n Vertical Airspeed = " + body.velocity.y;
+        }
     }
 }
